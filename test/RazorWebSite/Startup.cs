cdf42b94b187d5255fd0c5368f8a421cca8f4773
Thijs Brobbel <thijsbrobbel@gmail.com>
2024-04-28 23:15:17 +0200

    HACK: Add test case: approved inline style, with encoded html (the colon), approved via explicit hash in startup

--- test/RazorWebSite/Startup.cs
+++ test/RazorWebSite/Startup.cs
@@ -29,7 +29,7 @@ public class Startup
             .AddContentSecurityPolicy(builder =>
             {
                 builder.AddUpgradeInsecureRequests();
-                builder.AddDefaultSrc().Self(); 
+                builder.AddDefaultSrc().Self();
                 builder.AddConnectSrc().From("*");
                 builder.AddFontSrc().From("*");
                 builder.AddFrameAncestors().From("*");
@@ -39,7 +39,7 @@ public class Startup
                 builder.AddImgSrc().From("*").Data();
                 builder.AddObjectSrc().From("*");
                 builder.AddScriptSrc().From("*").UnsafeInline().UnsafeEval();
-                builder.AddStyleSrc().From("*").UnsafeEval().UnsafeInline();
+                builder.AddStyleSrc().From("*").UnsafeHashes().WithHash256("MCP66z4xZsFojgSzAEKSw3cor5mYnm49IoGrnIBfEO4="); //.UnsafeEval().UnsafeInline();
             })
             .RemoveServerHeader();
 
